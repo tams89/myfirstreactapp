1a6957206ad287a6b841e9d6f3e0dd9b8f204428
Tamesh Sivaguru <tam.s89@gmail.com>
2025-01-29 19:56:08 +0000

    Used cards & grid to style body
    Shows business in a grid of cards

--- src/components/Business/Business.jsx
+++ src/components/Business/Business.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import { Card, CardContent, CardActionArea, CardMedia, Typography, Grid2 } from '@mui/material';
 
-const Business = ({ business }) => {
+const Business = ({ business, key }) => {
+    const [selectedCard, setSelectedCard] = React.useState(0);
     return (
-        <span>
-            <h1>{business.name}</h1>
-            <img src={business.image} alt={business.name} />
-            <p>{business.address}</p>
-            <p>{business.city}, {business.state} {business.zipcode}</p>
-            <p>Category: {business.category}</p>
-            <p>Rating: {business.rating}</p>
-            <p>Reviews: {business.reviewCount}</p>
-        </span>
+        <Card variant='outline' sx={{ maxWidth: 345, height: '100%' }}>
+            <CardActionArea
+                onClick={() => setSelectedCard(key)}
+                data-active={selectedCard === key ? '' : undefined}
+                sx={{
+                    backgroundColor: 'rgba(255,255,255,0.80)',
+                    outline: 'none',
+                    height: '100%',
+                    '&[data-active]': {
+                        '&:hover': {
+                            backgroundColor: 'action.selectedHover',
+                        },
+                    },
+                }}
+            >
+                <CardContent sx={{ height: '95%', p: 1 }}>
+                    <CardMedia component="img" sx={{ objectFit: "cover" }} image={business.image} alt={business.name} />
+                    <br />
+                    <Typography variant="h5" component="div">{business.name}</Typography>
+                    <Grid2 container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        <Grid2 size={6}>
+                            <Typography variant="body2" color="text.secondary">
+                                <strong>Address: </strong><br />
+                                {business.address}<br />
+                                {business.city}, {business.state} {business.zipcode}
+                            </Typography>
+                        </Grid2>
+                        <Grid2 size={6}>
+                            <Typography variant="body2" color="text.secondary"><strong>Category:</strong> {business.category}</Typography>
+                            <Typography variant="body2" color="text.secondary"><strong>Rating:</strong> {business.rating}</Typography>
+                            <Typography variant="body2" color="text.secondary"><strong>Reviews:</strong> {business.reviewCount}</Typography>
+                        </Grid2>
+                    </Grid2>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 };
 
--- src/components/Business/BusinessList.jsx
+++ src/components/Business/BusinessList.jsx
@@ -1,40 +1,52 @@
 import React from 'react';
 import Business from './Business';
 import BusinessModel from '../../models/Business';
-
-const businesses = [
-    new BusinessModel(
-        'https://via.placeholder.com/150',
-        'Business One',
-        '123 Business St',
-        'Business City',
-        'BS',
-        '12345',
-        'Category One',
-        4.5,
-        90
-    ),
-    new BusinessModel(
-        'https://via.placeholder.com/150',
-        'Business Two',
-        '456 Business Ave',
-        'Business Town',
-        'BT',
-        '67890',
-        'Category Two',
-        4.0,
-        120
-    ),
-    // Add more businesses as needed
-];
+import { Box } from '@mui/material';
 
 const BusinessList = () => {
+    const businesses = [
+        new BusinessModel(
+            'https://www.hot-dinners.com/images/stories/features/2020/ott/amazonico.jpg',
+            'Business One',
+            '123 Business St',
+            'Business City',
+            'BS',
+            '12345',
+            'Category One',
+            4.5,
+            90
+        ),
+        new BusinessModel(
+            'https://media.cntraveller.com/photos/611bf740704875486571a300/4:3/w_2664,h_1998,c_limit/duck-waffle-conde-nast-traveller-17june21-pr.jpg',
+            'Business Two',
+            '456 Business Ave',
+            'Business Town',
+            'BT',
+            '67890',
+            'Category Two',
+            4.0,
+            120
+        ),
+        new BusinessModel(
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWSRqs9h9PiNCBiW8Jq7jNNiJ63A3nQ8QIWw&s',
+            'Business Three',
+            'A Business Ave',
+            'A Business Town',
+            'BX',
+            '58974',
+            'Category Three',
+            5.0,
+            1000000
+        ),
+        // Add more businesses as needed
+    ];
+
     return (
-        <span>
+        <Box sx={{ width: '100%', display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(min(20%, 100%), 1fr))', gap: 10, p: 1 }}>
             {businesses.map((business, index) => (
                 <Business key={index} business={business} />
             ))}
-        </span>
+        </Box>
     );
 };
 
