ab4f1737a0a2d3b9f6c043176c0b9730437d2a92
Tamesh Sivaguru <tam.s89@gmail.com>
2025-02-01 15:43:36 +0000

    Refactored to supply array of businesses from the top level app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import './App.css';
-import BusinessList from './components/Business/BusinessList';
-import SearchBar from './components/SearchBar/SearchBar';
-import { Container } from '@mui/material';
+import React from "react";
+import "./App.css";
+import BusinessList from "./components/Business/BusinessList";
+import SearchBar from "./components/SearchBar/SearchBar";
+import { Container } from "@mui/material";
+import BusinessModel from "././models/Business";
 
 function App() {
+  const businessArray = [
+    new BusinessModel(
+      "https://www.hot-dinners.com/images/stories/features/2020/ott/amazonico.jpg",
+      "Business One",
+      "123 Business St",
+      "Business City",
+      "BS",
+      "12345",
+      "Category One",
+      4.5,
+      90
+    ),
+    new BusinessModel(
+      "https://media.cntraveller.com/photos/611bf740704875486571a300/4:3/w_2664,h_1998,c_limit/duck-waffle-conde-nast-traveller-17june21-pr.jpg",
+      "Business Two",
+      "456 Business Ave",
+      "Business Town",
+      "BT",
+      "67890",
+      "Category Two",
+      4.0,
+      120
+    ),
+    new BusinessModel(
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWSRqs9h9PiNCBiW8Jq7jNNiJ63A3nQ8QIWw&s",
+      "Business Three",
+      "A Business Ave",
+      "A Business Town",
+      "BX",
+      "58974",
+      "Category Three",
+      5.0,
+      1000000
+    )
+    // Add more businesses as needed
+  ];
+
   return (
     <div>
       <SearchBar />
       <Container>
-        <BusinessList />
+        <BusinessList items={businessArray} />
       </Container>
     </div>
   );
--- src/components/Business/BusinessList.jsx
+++ src/components/Business/BusinessList.jsx
@@ -1,49 +1,12 @@
 import React from 'react';
 import Business from './Business';
-import BusinessModel from '../../models/Business';
 import { Box } from '@mui/material';
 
-const BusinessList = () => {
-    const businesses = [
-        new BusinessModel(
-            'https://www.hot-dinners.com/images/stories/features/2020/ott/amazonico.jpg',
-            'Business One',
-            '123 Business St',
-            'Business City',
-            'BS',
-            '12345',
-            'Category One',
-            4.5,
-            90
-        ),
-        new BusinessModel(
-            'https://media.cntraveller.com/photos/611bf740704875486571a300/4:3/w_2664,h_1998,c_limit/duck-waffle-conde-nast-traveller-17june21-pr.jpg',
-            'Business Two',
-            '456 Business Ave',
-            'Business Town',
-            'BT',
-            '67890',
-            'Category Two',
-            4.0,
-            120
-        ),
-        new BusinessModel(
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWSRqs9h9PiNCBiW8Jq7jNNiJ63A3nQ8QIWw&s',
-            'Business Three',
-            'A Business Ave',
-            'A Business Town',
-            'BX',
-            '58974',
-            'Category Three',
-            5.0,
-            1000000
-        ),
-        // Add more businesses as needed
-    ];
+const BusinessList = ({items}) => {
 
     return (
         <Box sx={{ width: '100%', display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(min(20%, 100%), 1fr))', gap: 10, p: 1 }}>
-            {businesses.map((business, index) => (
+            {items.map((business, index) => (
                 <Business key={index} business={business} />
             ))}
         </Box>
