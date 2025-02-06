d3634195a2dbe1f613304eb8630b52b9b0abd87b
Tamesh Sivaguru <tam.s89@gmail.com>
2025-02-06 18:57:06 +0000

    Map response from API and return

--- src/utils/ApiHelper.js
+++ src/utils/ApiHelper.js
@@ -1,15 +1,34 @@
 const fetchData = async (terms, location, sortBy) => {
-  const apiKey = process.env.YELP_APP_API_KEY;
+  const YELP_API_KEY = process.env.REACT_APP_YELP_API_KEY; // TODO this doesn't work
   const baseUrl = "https://api.yelp.com/v3";
   const endpoint = "/businesses/search";
-  const urlToFetch = `${baseUrl}${endpoint}?term=${terms}&location=${location}&sort_by=${sortBy}`;
+  const urlToFetch = `${cors}${baseUrl}${endpoint}?term=${terms}&location=${location}&sort_by=${sortBy}`;
   const response = await fetch(urlToFetch, {
-    headers: { Authorization: `Bearer ${apiKey}`, accept: "application/json" }
+    headers: { Authorization: `Bearer ${YELP_API_KEY}` }
   });
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
+
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    if (jsonResponse.businesses) {
+      const businesses = jsonResponse.businesses.map(business => {
+        return {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        };
+      });
+      return businesses;
+    }
   }
-  return response.json();
+
+  throw new Error(response);
 };
 
 export default fetchData;
